9d9629bd5807b00582c0545d382f70ab401e14c9
Samuel Adeshina <samueladeshina73@gmail.com>
2016-01-24 15:58:01 +0100

    Golang Owns This Space!

--- Main.java
+++
@@ -1,8 +0,0 @@
-class Main
-{
-	public static void main(String[] args)
-	{
-		Account acc = new Account();
-		System.out.println(acc.getName());
-	}
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Experiments
-Just my own way of finding out what programming languages are made of
